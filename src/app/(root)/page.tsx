4dcb45be6d8c1d2327030d5192abed6eec278c45
VIGNESH K <107059364+vigneshvicky2@users.noreply.github.com>
2025-06-26 12:43:52 +0530

    Update page.tsx

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -5,12 +5,11 @@ import InputPanel from "./_components/InputPanel";
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen overflow-x-auto hide-scrollbar">
       <div className="max-w-[1800px] mx-auto p-4">
         <Header />
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
           {/* Left Side: Editor */}
-
           <div className="min-h-[400px]">
             <EditorPanel />
           </div>
